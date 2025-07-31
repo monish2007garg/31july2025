657a7c3c76a720b443903d57727af7c7dd370a28
monish garg <monish2007garg@gmail.com>
2025-07-31 15:27:14 +0530

    Heya

---
+++ a.js
@@ -0,0 +1,31 @@
+// I can export anything 
+
+// Today i am going to export class 
+
+//1. Class Defination is one time Process 
+ // ClassName should written in PascalCase
+ //A is a Parent Class , It is also called BAse Class
+class A{
+    //1.property/Variable
+    a=10; //declaration and Initialisation 
+    //2.Constructor/Esp Method/function
+
+    //3.Method/Functions
+
+}
+//
+class B extends A{
+   //1.property/Variable
+   b=20;
+   //2.Constructor/Esp Method/function
+
+   //3.Method/Functions
+}
+
+// I want to export child class i.e B
+
+
+//There are 2 Types of js Export 
+//export default <something>
+export default B;
+//1. Default Export 
